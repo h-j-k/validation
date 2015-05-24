1218666618ee28e6bbb3602186c350622c722d12
h-j-k <han.junkwang@gmail.com>
2015-05-24 12:49:59 +0800

    Refactored unit test.

--- src/test/java/com/ikueb/validation/ValidatorTest.java
+++ src/test/java/com/ikueb/validation/ValidatorTest.java
@@ -1,5 +1,8 @@
 package com.ikueb.validation;
 
+import static com.ikueb.validation.Validator.check;
+import static com.ikueb.validation.Validator.filter;
+import static com.ikueb.validation.Validator.notNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -13,6 +16,7 @@ import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +27,10 @@ public class ValidatorTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ValidatorTest.class);
 
-    private static interface Widget {
+    public static interface Widget {
         boolean isActive();
         boolean isReady();
     }
-
-    private static final Predicate<Widget>[] RULES = Arrays.asList(Validator.notNull(),
-            w -> ((Widget) w).isActive(), w -> ((Widget) w).isReady()).toArray(
-            new Predicate[0]);
 
     enum TestWidget implements Widget {
         NULL, INACTIVE, NOT_READY, READY;
@@ -45,52 +45,30 @@ public class ValidatorTest {
             return this == READY;
         }
 
-        public void verify() {
-            Validator.check(this == NULL ? null : this, RULES)
-                        .ifPresent(TestWidget::assertReady);
+        private Widget get() {
+            return this == NULL ? null : this;
         }
 
-        public void verifyWithDefaultExceptionMessage() {
-            try {
-                Validator.check(this == NULL ? null : this, true, RULES)
-                            .ifPresent(TestWidget::assertReady);
-            } catch (IllegalStateException e) {
-                assertException(e, "Validation rule #" + (ordinal() + 1) + " failed.");
-            }
+        public String getFailureReason() {
+            return "Widget is " + name().toLowerCase().replace('_', ' ') + ".";
         }
+    }
 
-        public void verifyWithExceptionMessage() {
-            try {
-                Validator.check(this == NULL ? null : this, Arrays.asList(RULES),
-                                EnumSet.complementOf(EnumSet.of(READY)).stream()
-                                    .map(Object::toString).collect(Collectors.toList()))
-                            .ifPresent(TestWidget::assertReady);
-            } catch (IllegalStateException e) {
-                assertException(e, toString());
-            }
-        }
+    private static final Predicate<Widget>[] RULES = new Predicate[] { notNull(),
+            w -> ((Widget) w).isActive(), w -> ((Widget) w).isReady() };
+    private static final String[] REASONS = EnumSet
+            .complementOf(EnumSet.of(TestWidget.READY)).stream()
+            .map(TestWidget::getFailureReason).toArray(String[]::new);
+    private static final Map<Predicate<Widget>, String> RULE_MAP = IntStream.range(0, 3)
+            .collect(LinkedHashMap::new, (m, i) -> m.put(RULES[i], REASONS[i]), Map::putAll);
 
-        public void verifyWithMappedExceptionMessage() {
-            Map<Predicate<Widget>, String> ruleMap = IntStream.range(0, 3)
-                    .collect(LinkedHashMap::new, (map, i) -> map.put(RULES[i],
-                            TestWidget.values()[i].toString()), Map::putAll);
-            try {
-                Validator.check(this == NULL ? null : this, ruleMap)
-                            .ifPresent(TestWidget::assertReady);
-            } catch (IllegalStateException e) {
-                assertException(e, toString());
-            }
-        }
+    private static void assertReady(Widget widget) {
+        assertThat(widget, equalTo(TestWidget.READY));
+    }
 
-        private static void assertReady(Widget widget) {
-            assertThat(widget, equalTo(READY));
-        }
-
-        private static void assertException(IllegalStateException e,
-                String expectedMessage) {
-            assertThat(e.getMessage(), equalTo(expectedMessage));
-            logger.info("Verified for: " + e.getMessage());
-        }
+    private static void assertException(IllegalStateException e, String expected) {
+        assertThat(e.getMessage(), equalTo(expected));
+        logger.debug("Verified for: " + e.getMessage());
     }
 
     @DataProvider(name = "widget-test-cases")
@@ -101,34 +79,54 @@ public class ValidatorTest {
 
     @Test(dataProvider = "widget-test-cases")
     public void testNoException(TestWidget widget) {
-        widget.verify();
+        check(widget.get(), RULES).ifPresent(ValidatorTest::assertReady);
     }
 
     @Test(dataProvider = "widget-test-cases")
     public void testDefaultException(TestWidget widget) {
-        widget.verifyWithDefaultExceptionMessage();
+        try {
+            check(widget.get(), true, RULES).ifPresent(ValidatorTest::assertReady);
+        } catch (IllegalStateException e) {
+            String expected = "Validation rule #" + (widget.ordinal() + 1) + " failed.";
+            assertException(e, expected);
+        }
     }
 
     @Test(dataProvider = "widget-test-cases")
     public void testException(TestWidget widget) {
-        widget.verifyWithExceptionMessage();
+        try {
+            check(widget.get(), Arrays.asList(RULES), Arrays.asList(REASONS))
+                    .ifPresent(ValidatorTest::assertReady);
+        } catch (IllegalStateException e) {
+            assertException(e, widget.getFailureReason());
+        }
     }
 
     @Test(dataProvider = "widget-test-cases")
     public void testMappedException(TestWidget widget) {
-        widget.verifyWithMappedExceptionMessage();
+        try {
+            check(widget.get(), RULE_MAP).ifPresent(ValidatorTest::assertReady);
+        } catch (IllegalStateException e) {
+            assertException(e, widget.getFailureReason());
+        }
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testListsOfDifferentSizesThrows() {
-        Validator.check(TestWidget.NULL, Arrays.asList(RULES), Collections.emptyList());
+    @Test
+    public void testRuleCreation() {
+        check(TestWidget.READY, false, Stream.of(RULES).map(r -> Rule.of(r, null))
+                .collect(Collectors.toList())).ifPresent(ValidatorTest::assertReady);
     }
 
     @Test
     public void testFilter() {
-        assertThat(Validator.filter(EnumSet.allOf(TestWidget.class), HashSet::new,
-                Validator.notNull(), Widget::isActive, Widget::isReady),
+        assertThat(filter(EnumSet.allOf(TestWidget.class), HashSet::new,
+                notNull(), Widget::isActive, Widget::isReady),
                 equalTo(EnumSet.of(TestWidget.READY)));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testListsOfDifferentSizesThrows() {
+        check(TestWidget.READY, Arrays.asList(RULES), Collections.emptyList());
     }
 
     private static final String TEST = " TEST ";
@@ -140,6 +138,9 @@ public class ValidatorTest {
                 new Object[] { null, OTHER },
                 new Object[] { "", OTHER },
                 new Object[] { " ", OTHER },
+                new Object[] { "\t", OTHER },
+                new Object[] { "\n", OTHER },
+                new Object[] { "\t\n ", OTHER },
                 new Object[] { TEST, TEST.trim() }).iterator();
     }
 
